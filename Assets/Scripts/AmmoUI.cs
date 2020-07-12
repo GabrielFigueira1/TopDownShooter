f887b207e16bd263870a41ac4768a99d714574f9
Gabriel Figueira <gabrielfigueira98@gmail.com>
2020-07-12 15:15:09 -0400

    Ammo in the UI

---
+++ Assets/Scripts/AmmoUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class AmmoUI : MonoBehaviour
+{
+    [SerializeField] private GunStats gun; // Temporario
+    [SerializeField] private TextMeshProUGUI loadedAmmo;
+    [SerializeField] private TextMeshProUGUI extraAmmo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        loadedAmmo.text = gun.loadedAmmo.ToString();
+        extraAmmo.text = gun.extraAmmo.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        loadedAmmo.text = gun.loadedAmmo.ToString();
+        extraAmmo.text = gun.extraAmmo.ToString();
+    }
+}
