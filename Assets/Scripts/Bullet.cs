4e5cfe844224ad48e26abf93bfd98db484e0a8ac
Gabriel Figueira <gabrielfigueira98@gmail.com>
2020-07-14 16:16:26 -0400

    Bullets now use raycast

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -7,43 +7,54 @@ public class Bullet : MonoBehaviour
     public Rigidbody2D rb;
 
     private Vector2 shootAngle;
-    public float timeToDestroy;
-    public float speed = 10f;
+    public float timeToAutoDestroy = 5f;
+    public float speed;
 
     private GunStats gun;
+    private LayerMask solidLayer = 1 << 8 | 1 << 9;
     // Start is called before the first frame update
     void Start()
     {
         gun = PlayerCombat.activeWeapon; // encontra a referencia
         GetAngle();
         rb.velocity = shootAngle * speed;
-        Destroy(gameObject, timeToDestroy);
+        Destroy(gameObject, timeToAutoDestroy);
     }
 
     // Update is called once per frame
     void Update()
     {
-
     }
-    private void OnTriggerEnter2D(Collider2D other)
+    private void FixedUpdate()
     {
-        if (other.gameObject.tag.Equals("Player"))
-            return;
+        RaycastCollision();
 
-        if (other.gameObject.tag.Equals("Solid"))
-            Destroy(gameObject, 0.02f);
-
-        if (other.gameObject.tag.Equals("Enemy"))
-        {
-            var enemy = other.gameObject.GetComponent<EnemyStats>();
-            enemy.DoDamage(gun.damage);
-            Destroy(gameObject, 0.02f);
-        }
     }
 
     private void GetAngle()
     {
         shootAngle = transform.rotation * Vector2.right;
+        shootAngle.Normalize();
     }
 
+    private void RaycastCollision()
+    {
+        RaycastHit2D ray;
+        ray = Physics2D.Raycast(transform.position, shootAngle, speed * Time.fixedDeltaTime, solidLayer);
+        if (ray)
+        {
+            transform.position = ray.point;
+
+            if (ray.collider.tag.Equals("Solid"))
+                Destroy(gameObject, 0.02f);
+            if (ray.collider.tag.Equals("Enemy"))
+            {
+                var enemy = ray.collider.GetComponent<EnemyStats>();
+                enemy.DoDamage(gun.damage);
+                Destroy(gameObject, 0.02f);
+            }
+            Destroy(gameObject, 0.1f);
+            rb.velocity = Vector3.zero;
+        }
+    }
 }
